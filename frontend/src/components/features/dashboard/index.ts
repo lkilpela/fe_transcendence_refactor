22b4188f704c0b866a40a82c59c70ee5b4f0600f
Lumi Kilpeläinen <lumi.kilpelainen@wolt.com>
2025-06-16 23:42:20 +0300

    refactor: reorder exports in Dashboard component for improved clarity and consistency

--- frontend/src/components/features/dashboard/index.ts
+++ frontend/src/components/features/dashboard/index.ts
@@ -1,5 +1,5 @@
-export { default as PlayerManagement } from './PlayerManagement'
-export { default as GameStats } from './GameStats'
 export { default as MatchHistory } from './MatchHistory'
 export { default as QuickPlay } from './QuickPlay'
 export { default as TopPlayers } from './TopPlayers' 
+export { default as GameStats } from './GameStats'
+export { default as PlayerManagement } from './PlayerManagement'
