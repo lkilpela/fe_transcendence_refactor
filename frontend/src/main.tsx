5d778d7ac4c8f4045864f0fa7d0f43410aa11d4f
Lumi Kilpeläinen <lumi.kilpelainen@wolt.com>
2025-06-21 12:16:54 +0300

    feat: add placeholder avatar images and enable i18n configuration in main.tsx for improved localization support

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/styles/index.css'
 
-// import './i18n/config'
+import './i18n/config'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
